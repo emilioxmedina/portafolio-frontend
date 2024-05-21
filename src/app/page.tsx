ecc6e0bdbcf991ff2f258e1a472cc2f737690d81
emilioxmedina <medinamencos12@gmail.com>
2024-05-20 21:54:20 -0600

    proyects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import profilePic from "../../public/images/persona icono.jpg"
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,8 +14,30 @@ export default function Home() {
           <Image src={profilePic} height={200} width={200} alt="Foto de autor" className="rounded-full"/>
         </div>
       </div>
-      <div>
-        <h1>prueba</h1>
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="m-4 p-16 w-auto bg-gradient-to-b from-gray-900 via-gray-500 to-gray-900 rounded-3xl">
+          <h1 className="text-6xl font-bold font-serif text-center">PROYECTOS</h1>
+          <div className="flex flex-row gap-4">
+            <div className="w-1/3 flex flex-col justify-center items-center gap-3 m-2 mt-16 p-6 bg-gradient-to-r from-blue-900 via-slate-600 to-blue-950 rounded-3xl transition ease-in-out duration-200 hover:-translate-y-1 hover:scale-105">
+              <Image src={profilePic} height={200} width={200} alt="Foto de autor" className="rounded-3xl m-auto mb-4"/>
+              <h2 className="text-4xl font-bold text-center">Proyecto 1</h2>
+              <p>Texto descriptivo del proyecto 1.</p>
+              <Link href="#" className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-xl hover:bg-blue-300">ver al repositorio</Link>
+            </div>
+            <div className="w-1/3 flex flex-col justify-center items-center gap-3 m-2 mt-16 p-6 bg-gradient-to-r from-blue-900 via-slate-600 to-blue-950 rounded-3xl transition ease-in-out duration-200 hover:-translate-y-1 hover:scale-105">
+              <Image src={profilePic} height={200} width={200} alt="Foto de autor" className="rounded-3xl m-auto mb-4"/>
+              <h2 className="text-4xl font-bold text-center">Proyecto 2</h2>
+              <p>Texto descriptivo del proyecto 2.</p>
+              <Link href="#" className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-xl hover:bg-blue-300">ver al repositorio</Link>
+            </div>
+            <div className="w-1/3 flex flex-col justify-center items-center gap-3 m-2 mt-16 p-6 bg-gradient-to-r from-blue-900 via-slate-600 to-blue-950 rounded-3xl transition ease-in-out duration-200 hover:-translate-y-1 hover:scale-105">
+              <Image src={profilePic} height={200} width={200} alt="Foto de autor" className="rounded-3xl m-auto mb-4"/>
+              <h2 className="text-4xl font-bold text-center">Proyecto 3</h2>
+              <p>Texto descriptivo del proyecto 3.</p>
+              <Link href="#" className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-xl hover:bg-blue-300">ver al repositorio</Link>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
       );
