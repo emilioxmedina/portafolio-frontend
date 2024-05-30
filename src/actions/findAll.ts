8c74eb0ad17a2805b06aed29059700131431d1a3
emilioxmedina <medinamencos12@gmail.com>
2024-05-29 20:42:37 -0600

    Post

--- src/actions/findAll.ts
+++ src/actions/findAll.ts
@@ -1,6 +1,17 @@
 import baseAxios from "./baseAxios";
 
+type Post = {
+    data: [
+        {
+            attributes: {
+                Title: string;
+                Body: string;
+            }
+        }
+    ]
+}
+
 export default async function findAll() {
-    const response = await baseAxios.get(`/posts`);
+    const response = await baseAxios.get<Post>(`/posts`);
     return response.data;
 }
